3ebcc492735bf6b6e8b0ea085d071cf019001d79
Daniel Felps <daniel.l.felps@nga.mil>
2022-12-09 13:34:30 -0500

    one and two down

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String) : List<Int> {
+        fun getNext(rest: List<Int?>): Pair<Int,List<Int?>> {
+            val next = rest.takeWhile {it != null} as List<Int>
+            val temp = rest.dropWhile {it != null}
+            val rest = if (temp.isEmpty()) {
+                temp
+            } else {
+                temp.drop(1) //drop null
+            }
+            return Pair(next.sum(), rest)
+        }
+
+        var rest = input.lines().map {it.toIntOrNull()}
+        val sums = mutableListOf<Int>()
+
+        while (rest.isNotEmpty()) {
+            val (sum, temp) = getNext(rest)
+            rest = temp
+            sums.add(sum)
+        }
+
+        return sums
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+        return data.max()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input:String): Int {
+        val data = parseInput(input)
+        return data
+            .sortedDescending()
+            .take(3)
+            .sum()
+    }
 
-    val input = readInput("Day01")
+    val input = File("src/Day01.txt").readText()
+
     println(part1(input))
     println(part2(input))
+
+
 }
